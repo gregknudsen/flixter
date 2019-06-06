9c13bf1ca95dbdd450615b82df77355581fa4e12
Knudsen <gknudsen@duqlight.com>
2019-06-06 11:02:02 -0400

    add sorting for sections

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,12 +1,12 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_section, only: [:new, :create]
+  before_action :require_authorized_for_current_section, only: [:create]
   before_action :require_authorized_for_current_lesson, only: [:update]
 
-  def new
-    @section = Section.find(params[:section_id])
-    @lesson = Lesson.new
-  end
+  # def new
+  #   @section = Section.find(params[:section_id])
+  #   @lesson = Lesson.new
+  # end
 
   def create
     @lesson = current_section.lessons.create(lesson_params)
@@ -19,16 +19,17 @@ class Instructor::LessonsController < ApplicationController
   end
 
   private
-
+  
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
+  
   def require_authorized_for_current_lesson
     if current_lesson.section.course.user != current_user
       render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
